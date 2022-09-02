598bf5c2bf4d682c06df9c95fc3e255c0b061eeb
vural <https://github.com/vuralxm/GitProje1.git>
2022-09-02 21:46:23 +0300

    Framework added 1

--- src/test/java/Pages/GP18Content.java
+++ src/test/java/Pages/GP18Content.java
@@ -2,6 +2,6 @@ package Pages;
 
 public class GP18Content {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Worldd");
     }
 }
