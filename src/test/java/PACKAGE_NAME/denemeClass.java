fc15a11be3f53606ffff4bf516e323d972a54c61
HasanYonden <yondenhasan@gmail.com>
2022-09-02 19:58:33 +0200

    deneme5

--- src/test/java/PACKAGE_NAME/denemeClass.java
+++ src/test/java/PACKAGE_NAME/denemeClass.java
@@ -1,6 +1,6 @@
 package PACKAGE_NAME;public class denemeClass {
     public static void main(String[] args) {
-        System.out.println("Deneme 5");
+        System.out.println("Deneme 55");
 
 
     }
