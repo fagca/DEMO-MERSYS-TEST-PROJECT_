d3cb544c992fc6870c8221999f80db63b924b6ac
esrag <esragurevin@gmail.com>
2022-09-09 19:13:17 -0400

    GP20Content scenario 1 completed.

--- src/test/java/StepDefinitions/GP20Steps.java
+++ src/test/java/StepDefinitions/GP20Steps.java
@@ -8,14 +8,18 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import net.bytebuddy.utility.RandomString;
+import org.apache.commons.lang.RandomStringUtils;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Random;
 
 public class GP20Steps {
+    String randomName= RandomStringUtils.randomAlphabetic(8);
     GP20Content dc = new GP20Content();
 
 
@@ -27,11 +31,7 @@ public class GP20Steps {
 
     @When("Enter username and password and click login button")
     public void enterUsernameAndPasswordAndClickLoginButton() {
-        // waitleri var
-//        dc.username.sendKeys("richfield.edu");
-//        dc.password.sendKeys("Richfield2020!");
-//        dc.loginButton.click();
-        // 1 .aşama
+
         dc.findAndSend("username", "richfield.edu");
         dc.findAndSend("password", "Richfield2020!");
         dc.findAndClick("loginButton");
@@ -39,8 +39,7 @@ public class GP20Steps {
 
     @Then("User should login successfuly")
     public void userShouldLoginSuccessfuly() {
-//        dc.waitUntilVisible(dc.dashboard);
-//        Assert.assertTrue(dc.dashboard.getText().contains("Dashboard"));
+
 
         dc.findAndContainsText("dashboard", "Dashboard");
         dc.findAndClick("acceptCookies");
@@ -52,19 +51,78 @@ public class GP20Steps {
         dc.findAndClick("parameters");
         dc.findAndClick("nationalities");
 
-        GWD.Bekle(3);
+
+    }
+
+    @When("User create a Nationality name as")
+    public void userCreateANationalityNameAs() {
+        GWD.Bekle(1);
         dc.findAndClick("addbutton");
+        dc.findAndSend("name1",randomName);
+
+
     }
 
-    @When("Enter user a Nationality name as")
-    public void enterUserANationalityNameAs() {
-        dc.findAndSend("name", "esra1");
+   @Then("User should click save buttons")
+    public void userShouldClickSaveButtons(DataTable elements) {
+        List<String> listElement = elements.asList(String.class);
+
+        for (int i = 0; i < listElement.size(); i++) {
+            dc.findAndClick(listElement.get(i));
+
+        }
+
+
     }
 
+    @When("User should enter name")
+    public void userShouldEnterName() {
+      GWD.Bekle(1);
+        dc.findAndSend("searchText",randomName);
+       // List<List<String>> listElement = elements.asLists(String.class);
 
-    @Then("User should click save buttons")
-    public void userShouldClickSaveButtons() {
-        dc.findAndClick("save");
+        //for (int i = 0; i < listElement.size(); i++)
+         //   dc.findAndSend(listElement.get(i).get(0), listElement.get(i).get(1));
+
+
+    }
+
+    @And("User should search button")
+    public void userShouldSearchButton(DataTable elements) {
+        GWD.Bekle(1);
+        List<String> listElement = elements.asList(String.class);
+
+        for (int i = 0; i < listElement.size(); i++) {
+           dc.findAndClick(listElement.get(i));
+        }
+   }
+
+
+    @And("User should click edit buttons")
+    public void userShouldClickEditButtons(DataTable elements) {
+        GWD.Bekle(1);
+       List<String> listElement = elements.asList(String.class);
+
+       for (int i = 0; i < listElement.size(); i++) {
+           dc.findAndClick(listElement.get(i));
+        }
+    }
+
+    @And("Success messeage sould be displayed")
+    public void successMesseageSouldBeDisplayed() {
+        dc.findAndContainsText("success","success");
+    }
+
+    @And("User should click delete buttons")
+    public void userShouldClickDeleteButtons() {
+        GWD.Bekle(2);
+        dc.findAndClick("deleteButton");
+        dc.findAndClick("deleteButton2");
+//        List<String> listElement = elements.asList(String.class);
+//
+//        for (int i = 0; i < listElement.size(); i++) {
+//            dc.findAndClick(listElement.get(i));
+        //}
     }
 }
 
